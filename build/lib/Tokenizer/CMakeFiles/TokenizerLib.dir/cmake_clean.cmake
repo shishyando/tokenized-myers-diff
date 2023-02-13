4ca93abd4e80f14727c5071ca23b3097104c4cfc
Kamil Ninevskiy <ninevskykn@gmail.com>
2023-02-13 21:12:35 +0300

    bugfix

---
+++ build/lib/Tokenizer/CMakeFiles/TokenizerLib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TokenizerLib.dir/Tokenizer.cpp.o"
+  "CMakeFiles/TokenizerLib.dir/Tokenizer.cpp.o.d"
+  "libTokenizerLib.a"
+  "libTokenizerLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TokenizerLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
