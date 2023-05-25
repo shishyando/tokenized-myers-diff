2cd5224d6a96fedbe3a4d010b6338a227f9ca4c5
Kamil Ninevskiy <ninevskykn@gmail.com>
2023-05-25 19:32:06 +0300

    Add installer for code-semantic and README updating

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tokenized-myers-diff
+
+## Semantic code tokenization
+Use `--tokenizer semantic-code` option to tokenize code files using [Language Sever][https://microsoft.github.io/language-server-protocol/]:
+![semantic code example](media/code-semantic.gif)
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        apt install clangd-12
+        apt install qtbase5-dev qt5-qmake
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install clangd-12
+        brew install qt@5
+else
+        echo "Sorry, your platform isn't supported yet :("
+fi
