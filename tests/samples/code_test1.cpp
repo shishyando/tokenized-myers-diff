e9a241f5d80e70e53f8c77f3a981a947c141b197
Nikita Golubev <nikita.golubev08@gmail.com>
2023-05-26 15:53:12 +0300

    Fixed code-tokenizer for mac os

---
+++ tests/samples/code_test1.cpp
@@ -0,0 +1,6 @@
+#include <vector>
+
+int main() {
+    std::vector<int> a;
+    std::vector<int> b;
+}
---
+++ tests/samples/code_test2.cpp
@@ -0,0 +1,7 @@
+#include <vector>
+#include <set>
+
+signed main() {
+    std::vector<long long> a;
+    std::set<int> b;
+}
